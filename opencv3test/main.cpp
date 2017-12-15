2b91317d09512a2e6b91cad47380b101538ec01a
exesax <exesa@yandex.ru>
2017-12-15 17:01:05 +0500

    add skewing

--- opencv3test/main.cpp
+++ opencv3test/main.cpp
@@ -1,11 +1,12 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/core/ocl.hpp"
-#include <iostream>
-#include "time.h"
 #include "opencv2/core/core.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
+
+#include <ctime>
 #include <iostream>
+#include "time.h"
 
 #include <Windows.h>
 
@@ -95,7 +96,7 @@ bool matchWithPattern(Mat frame, Mat pattern, Rect roi, double detectionThreshol
 	absdiff(frameRoi, pattern, diff);
 	//imshow("diff", diff);
 	double diffSum = mean(diff)[0];
-	cout << diffSum << " ";
+	//cout << diffSum << " ";
 	return diffSum < detectionThreshold;
 }
 
@@ -106,14 +107,35 @@ void savePattern(Mat frame, Rect roi, int binThreshold, string filename) {
 }
 
 string OCR(tesseract::TessBaseAPI* api, Mat image) {
-	//Pix* tessImg = pixRead("try.png");
-	imwrite("C:\\Projects\\owtracker\\x64\\Debug\\temp.bmp", image);
+	clock_t functime = clock();
+
+	Point2f orig[4];
+	Point2f conv[4];
+
+	orig[0] = Point2f(18, 0);
+	orig[1] = Point2f(116, 0);
+	orig[2] = Point2f(102, 59);
+	orig[3] = Point2f(0, 59);
+
+	conv[0] = Point2f(0, 0);
+	conv[1] = Point2f(102, 0);
+	conv[2] = Point2f(102, 59);
+	conv[3] = Point2f(0, 59);
+	Mat trasnformMatrix = getPerspectiveTransform(orig, conv);
+
+	Mat unskewed;
+	warpPerspective(image, unskewed, trasnformMatrix, Size(image.cols, image.rows));
+	imshow("skew", unskewed);
+	imwrite("C:\\Projects\\owtracker\\x64\\Debug\\temp.bmp", unskewed);
+	//cout << "OCR write image: " << double(clock() - functime) / CLOCKS_PER_SEC << endl;
 	Pix* tesImg = pixRead("C:\\Projects\\owtracker\\x64\\Debug\\temp.bmp");
+	//cout << "OCR read image: " << double(clock() - functime) / CLOCKS_PER_SEC << endl;
 	api->SetImage(tesImg);
-	//api->SetImage(tessImg);
+	//cout << "OCR set image: " << double(clock() - functime) / CLOCKS_PER_SEC << endl;
 	api->SetVariable("classify_bln_numeric_mode", "1");
+	//cout << "OCR set var: " << double(clock() - functime) / CLOCKS_PER_SEC << endl;
 	string outText = string(api->GetUTF8Text());
-	//printf("OCR:\n%s", outText);
+	//cout << "OCR full: " << double(clock() - functime) / CLOCKS_PER_SEC << endl;
 	return outText;
 }
 
@@ -176,9 +198,15 @@ int main(int argc, char** argv) {
 
 	
 
-	tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+	tesseract::TessBaseAPI *owTess = new tesseract::TessBaseAPI();
+	tesseract::TessBaseAPI *engTess = new tesseract::TessBaseAPI();
 
-	if (api->Init(NULL, "owdig")) {
+	if (owTess->Init(NULL, "eng")) {
+		fprintf(stderr, "Could not init tesseract\n");
+		exit(1);
+	}
+
+	if (engTess->Init(NULL, "eng")) {
 		fprintf(stderr, "Could not init tesseract\n");
 		exit(1);
 	}
@@ -188,12 +216,21 @@ int main(int argc, char** argv) {
 	Mat scoreboardPattern = openImage("C:\\Projects\\owtracker\\x64\\Debug\\scoreboard_pattern.png");
 	Mat loadingPattern = openImage("C:\\Projects\\owtracker\\x64\\Debug\\loading_pattern.png");
 	Mat menuPattern = openImage("C:\\Projects\\owtracker\\x64\\Debug\\menu_still_pattern.png");
+
+	
+
+	
+
+	
 	
 	while (true) {
 		/*bool ok = vid.read(currentFrame);
 		if (!ok) break;*/
 
+		Sleep(500);
+
 		currentFrame = hwnd2mat(hDesktopWnd);
+		//imshow("img", currentFrame);
 		//currentFrame = openImage("C:\\Projects\\owtracker\\x64\\Debug\\currentFrame.png");
 		//cout << currentFrame.cols << " " << currentFrame.rows << endl;
 
@@ -212,12 +249,12 @@ int main(int argc, char** argv) {
 		//bool isScoreboard = false;
 		//bool isLoading = false;
 		//bool isMenu = false;
-		cout << endl;
+		//cout << endl;
 		if (isSr) {
 			cout << "sr screen found" << endl;
 			
 			Mat toOcr = get_sr(currentFrame);
-			string outText = OCR(api, toOcr);
+			string outText = OCR(owTess, toOcr);
 			cout << outText << endl;
 		}
 		if (isScoreboard) {
@@ -225,9 +262,15 @@ int main(int argc, char** argv) {
 			string outText[7];
 			Mat scores[7];
 			get_scores(currentFrame, scores);
-			for (int i = 0; i < 7; i++) {
-				outText[i] = OCR(api, scores[i]);
-			}
+			//for (int i = 0; i < 7; i++) {
+			outText[0] = OCR(owTess, scores[0]);
+			outText[1] = OCR(owTess, scores[1]);
+			outText[2] = OCR(owTess, scores[2]);
+			outText[3] = OCR(owTess, scores[3]);
+			outText[4] = OCR(owTess, scores[4]);
+			outText[5] = OCR(owTess, scores[5]);
+			outText[6] = OCR(engTess, scores[6]);
+			//}
 
 			cout << "ELIM " << outText[0] << endl;
 			cout << "OBJ " << outText[1] << endl;
@@ -242,8 +285,8 @@ int main(int argc, char** argv) {
 			string outText[2];
 			Mat teams_sr[2];
 			get_teams_sr(currentFrame, teams_sr);
-			outText[0] = OCR(api, teams_sr[0]);
-			outText[1] = OCR(api, teams_sr[1]);
+			outText[0] = OCR(owTess, teams_sr[0]);
+			outText[1] = OCR(owTess, teams_sr[1]);
 			cout << outText[0] << endl;
 			cout << outText[1] << endl;
 
@@ -252,7 +295,7 @@ int main(int argc, char** argv) {
 		if (isMenu) {
 			cout << "menu screen found" << endl;
 			Mat toOcr = get_menu_sr(currentFrame);
-			string outText = OCR(api, toOcr);
+			string outText = OCR(engTess, toOcr);
 			cout << outText << endl;
 		}
 
